6b2c899e026adba10040b1f63e14fcc208de6e6c
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-19 13:28:44 +0400

    Add files via upload

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,7 @@ import { DurableObject } from "cloudflare:workers";
 import { buildPushPayload } from "@block65/webcrypto-web-push";
 
 const VERSION = "3.8.7";
-const RELEASE_ID = "3.8.7-calendar-r10";
+const RELEASE_ID = "3.8.7-calendar-r11";
 const UPDATE_SIGNAL_VERSION = "3.8.7\u200B";
 const PREVIOUS_PUBLISHED_VERSION = "3.8.6";
 const PREVIOUS_RELEASE_ID = "3.8.6";
@@ -13,7 +13,8 @@ const ACCIDENTAL_PREPUBLISH_RELEASE_IDS = new Set([
   "3.8.7-calendar-r6",
   "3.8.7-calendar-r7",
   "3.8.7-calendar-r8",
-  "3.8.7-calendar-r9"
+  "3.8.7-calendar-r9",
+  "3.8.7-calendar-r10"
 ]);
 const SESSION_MS = 10 * 365 * 24 * 60 * 60 * 1000;
 const PBKDF2_ITERATIONS = 100000;

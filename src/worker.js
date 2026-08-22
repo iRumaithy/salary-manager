afd56ea17d094f403ee2e4e0e2c45ec80d07b6e6
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-22 16:14:52 +0400

    Add files via upload

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,7 @@ import { DurableObject } from "cloudflare:workers";
 import { buildPushPayload } from "@block65/webcrypto-web-push";
 
 const VERSION = "3.8.9";
-const RELEASE_ID = "3.8.9-deposits-shared-r3";
+const RELEASE_ID = "3.8.9-deposits-shared-r4";
 const UPDATE_SIGNAL_VERSION = "3.8.9\u200B";
 const PREVIOUS_PUBLISHED_VERSION = "3.8.6";
 const PREVIOUS_RELEASE_ID = "3.8.6";
@@ -25,7 +25,8 @@ const ACCIDENTAL_PREPUBLISH_RELEASE_IDS = new Set([
   "3.8.8-shared-expenses-r2",
   "3.8.8-shared-expenses-r3",
   "3.8.9-shared-expenses-r1",
-  "3.8.9-shared-expenses-r2"
+  "3.8.9-shared-expenses-r2",
+  "3.8.9-deposits-shared-r3"
 ]);
 const SESSION_MS = 10 * 365 * 24 * 60 * 60 * 1000;
 const PBKDF2_ITERATIONS = 100000;

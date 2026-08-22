1ac4637ddb9c51fbaa74ef1e0cc5d90c2902baf1
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-22 21:11:46 +0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "salary-manager-v3.8.9-deposits-edit-r5";
+const CACHE = "salary-manager-v3.8.9-time-order-r6";
 const SCOPE = self.registration.scope;
 const SHELL_KEY = new URL("__salary_manager_app_shell__", SCOPE).href;
 const STATIC_CORE = [

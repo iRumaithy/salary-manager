52a4f25618a0a2b40137a63042ac0e09749a5bff
iRumaithy <alromaithi.3bo0d@hotmail.com>
2026-08-23 14:16:23 +0400

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "salary-manager-v3.8.9-header-time-r7";
+const CACHE = "salary-manager-v3.9.0-expense-sign-r1";
 const SCOPE = self.registration.scope;
 const SHELL_KEY = new URL("__salary_manager_app_shell__", SCOPE).href;
 const STATIC_CORE = [
